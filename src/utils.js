c240ad306d76af19b3c9f3342731fa73e0161bae
Viliam Simko <viliam.simko@gmail.com>
2018-12-05 10:37:37 +0100

    fix: function `mergeConfig` now uses `deepCopy` through JSON serdes. Before we created only a shallow copy using the spread operator and we also passed arrays by reference.

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,49 @@
-const mergeConfig = (a, b) => mergeConfigImpure({ ...a }, b)
+// @ts-check
+/**
+ * Merges `b` configuration structure to `a` by creating
+ * a deep copy of `a`.
+ * - Note: The configuration structure `a` must be serializable to JSON.
+ * - This function is pure
+ *
+ * @template T
+ * @param {T} a
+ * @param {T} b
+ */
+const mergeConfig = (a, b) => mergeConfigImpure(deepCopy(a), b)
 
+/**
+ * Creates a deep copy of an object using JSON seriazlie/deserialize.
+ * This function is pure
+ * @template T
+ * @param {T} x Must be serializable to JSON!
+ * @return {T}
+ */
+const deepCopy = x => JSON.parse(JSON.stringify(x))
+
+/**
+ * Merges `b` to `a` by altering `a`.
+ * @template T
+ * @param {T} target
+ * @param {T} source
+ */
 function mergeConfigImpure (target, source) {
   for (let k in source) {
-    const objOrScalar = target[k]
-    if (objOrScalar != null && objOrScalar.constructor === Object) {
-      mergeConfigImpure(objOrScalar, source[k]) // recurse on objects
+    const maybeObject = target[k]
+    if (maybeObject != null && maybeObject.constructor === Object) {
+      mergeConfigImpure(maybeObject, source[k]) // recurse on objects
     } else {
-      target[k] = source[k] // assign scalar value
+      target[k] = deepCopy(source[k]) // assign scalar value
     }
   }
   return target
 }
 
-/** @type {(x:object) => object|object[]} */
-const ensureArray = x => (Object(x) instanceof Array ? x : [x])
+/**
+ * @template T
+ * @param {T | T[]} x
+ * @return {T[]}
+ */
+const ensureArray = x => x instanceof Array ? x : [x]
 
 const isEmptyObject = obj =>
   Object.keys(obj).length === 0 && obj.constructor === Object
