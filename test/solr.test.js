9dcc15530e2e87abd22bceb79b52c64a416d8498
Viliam Simko <viliam.simko@gmail.com>
2018-12-08 13:26:25 +0100

    test: add tests that use the solr docker container

--- test/solr.test.js
+++ test/solr.test.js
@@ -1,11 +1,54 @@
-const { prepareSolrClient } = require("../src/index")
-const { expect } = require("chai")
+// @ts-check
+const { prepareSolrClient, prepareCoreAdmin } = require("../src/index")
+const { expect, use } = require("chai")
+const cap = require("chai-as-promised")
+use(cap)
+
+/** @typedef {import("../src/solr").SolrConfig} SolrConfig */
+
+/** @type {SolrConfig} */
+const testConfing = { port: 18983 } // see docker-compose.yml
+
+describe("#prepareCoreAdmin", async () => {
+  const admin = prepareCoreAdmin(testConfing)
+
+  it("should ping core", () => {
+    admin.ping("testcore")
+    expect(admin.ping("non-existent-core")).to.be.rejectedWith("Not Found")
+  })
+
+  it("should delete core", () => {
+    expect(admin.solrDeleteCore("non-existent-core")).to.be.rejectedWith(
+      "Cannot unload non-existent core"
+    )
+  })
+})
 
 describe("#prepareSolrClient", () => {
-  it("should be able to prepare the client", () => {
-    const solr = prepareSolrClient({ core: "dummy" })
-    expect(solr).to.haveOwnProperty("ping")
-    expect(solr).to.haveOwnProperty("query")
-    expect(solr).to.haveOwnProperty("add")
+  const solr = prepareSolrClient("testcore", testConfing)
+
+  it("should query documents", async () => {
+    await solr.query({ query: "*:*" })
+  })
+
+  it("should delete documents", async () => {
+    await solr.delete({ id: "123" })
+  })
+
+  it("should add documents and commit", async () => {
+    await solr.add({ id: "123", hello: "world" })
+    await solr.commit()
+  })
+
+  it("should provide other functions", () => {
+    expect(solr).to.haveOwnProperty("deleteField")
+    expect(solr).to.haveOwnProperty("mergedConfig")
+    expect(solr).to.haveOwnProperty("deleteFieldType")
+    expect(solr).to.haveOwnProperty("addField")
+    expect(solr).to.haveOwnProperty("addFieldType")
+    expect(solr).to.haveOwnProperty("replaceField")
+    expect(solr).to.haveOwnProperty("config")
+    expect(solr).to.haveOwnProperty("solrListFields")
+    expect(solr).to.haveOwnProperty("configAutoEnableFields")
   })
 })
